651ddeca2d9b4e14221259534fb91baf4dfe60d5
Nabila Naaz <naaznabila217@gmail.com>
2026-06-29 09:41:26 +0530

    Add files via upload

---
+++ ASCII.py
@@ -0,0 +1,20 @@
+'''for i in range(4):
+    
+    l=int(input("enter a number:"))
+    if 0 < l < 127:
+        print("alphabet:",chr(l))
+    else:
+        print("invalid number!")'''
+        
+        
+        
+        
+l=int(input("enter a number:"))
+m=int(input("enter a number:"))
+n=int(input("enter a number:"))
+o=int(input("enter a number:"))
+print(l,"-",chr(l))
+print(m,"-",chr(m))
+print(n,"-",chr(n))
+print(o,"-",chr(o))
+        
